7b25008ffe287d8a74e40c603106d8000b5ff089
Sanskriti <asanskriti93@gmail.com>
2026-05-17 17:16:12 +0530

    a

--- frontend/src/components/Charts.jsx
+++ frontend/src/components/Charts.jsx
@@ -4,24 +4,45 @@ import {
   XAxis,
   YAxis,
   Tooltip,
+  CartesianGrid,
   ResponsiveContainer,
 } from "recharts";
 
 const Charts = ({ data }) => {
+
   return (
-    <div className="bg-slate-800 p-6 rounded-2xl mt-6">
-      <h2 className="text-2xl font-semibold mb-5">
+
+    <div className="bg-slate-800 p-6 rounded-2xl mt-10">
+
+      <h2 className="text-3xl font-bold text-white mb-6">
         Match Analytics
       </h2>
 
-      <ResponsiveContainer width="100%" height={300}>
-        <BarChart data={data}>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="finalScore" fill="#3b82f6" />
-        </BarChart>
-      </ResponsiveContainer>
+      <div style={{ width: "100%", height: 400 }}>
+
+        <ResponsiveContainer>
+
+          <BarChart data={data}>
+
+            <CartesianGrid strokeDasharray="3 3" />
+
+            <XAxis dataKey="name" />
+
+            <YAxis />
+
+            <Tooltip />
+
+            <Bar
+              dataKey="finalScore"
+              fill="#3b82f6"
+            />
+
+          </BarChart>
+
+        </ResponsiveContainer>
+
+      </div>
+
     </div>
   );
 };
