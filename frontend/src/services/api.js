74431ae4bcb6427e925a1a6cf764fa1bf21120c1
Sanskriti <asanskriti93@gmail.com>
2026-05-17 16:16:08 +0530

    Frontend deployment setup

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ats-backend-080t.onrender.com/api",
 });
 
 export default API;
